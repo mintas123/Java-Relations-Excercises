f8461cc59d694ae9fbb880a56211bc4685ba5b8b
Jakub Mróz <kumroz@gmail.com>
2020-08-19 20:13:58 +0200

    patient search autocomplete, patient-details added"

---
+++ FinalProject/mas_FP/front/src/app/patient-search/patient-detail/patient-detail.component.ts
@@ -0,0 +1,18 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {Patient} from '../../model/patient';
+
+@Component({
+  selector: 'app-patient-detail',
+  templateUrl: './patient-detail.component.html',
+  styleUrls: ['./patient-detail.component.css']
+})
+export class PatientDetailComponent implements OnInit {
+
+  @Input() patient: Patient;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- FinalProject/mas_FP/front/src/app/patient-search/patient-search.component.ts
+++ FinalProject/mas_FP/front/src/app/patient-search/patient-search.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl} from '@angular/forms';
+import {Observable} from 'rxjs';
+import {map, startWith} from 'rxjs/operators';
+import {Patient} from '../model/patient';
 
 @Component({
   selector: 'app-patient-search',
@@ -7,9 +11,64 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PatientSearchComponent implements OnInit {
 
-  constructor() { }
+  selectedPatient: Patient;
+  myControl = new FormControl();
+  options: Patient[] = [
+    new Patient('Jakub Mróz', 99042608556,
+      true, 'kumroz@gmail.com', '787312931',
+      new Date(1999, 3, 26)),
+    new Patient('Jakub Mróż', 99042608123,
+      true, 'kumroz@gmail.com', '787312931',
+      new Date(1999, 3, 26)),
+    new Patient('Juba Mruz', 99042234656,
+      true, 'kumroz@gmail.com', '787312931',
+      new Date(1999, 3, 26)),
+    new Patient('AAAA Mróz', 99042608556,
+      true, 'kumroz@gmail.com', '787312931',
+      new Date(1999, 3, 26)),
+    new Patient('AAac Mróż', 99042608123,
+      true, 'kumroz@gmail.com', '787312931',
+      new Date(1999, 3, 26)),
+    new Patient('Acc Mruz', 99042234656,
+      true, 'kumroz@gmail.com', '787312931',
+      new Date(1999, 3, 26)),
+    new Patient('Aeee Mróz', 99042608556,
+      true, 'kumroz@gmail.com', '787312931',
+      new Date(1999, 3, 26)),
+    new Patient('AWE Mróż', 99042608123,
+      true, 'kumroz@gmail.com', '787312931',
+      new Date(1999, 3, 26)),
+    new Patient('EEE Mruz', 99042234656,
+      true, 'kumroz@gmail.com', '787312931',
+      new Date(1999, 3, 26)),
+  ];
+  filteredOptions: Observable<Patient[]>;
 
-  ngOnInit(): void {
+  constructor() {
   }
 
+  ngOnInit() {
+    this.initFilter();
+  }
+
+  initFilter(){
+    this.filteredOptions = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value))
+    );
+  }
+  selectPatient(value){
+    this.selectedPatient = value;
+  }
+
+  displayFn(subject) {
+    return subject ? subject.name : undefined;
+  }
+
+  private _filter(value: string): Patient[] {
+    const filterValue = value.toLowerCase();
+    if (value.length < 3) { return null; }
+
+    return this.options.filter(
+      option => option.name.toLowerCase().includes(filterValue) || option.pesel.toString().indexOf(filterValue) === 0); }
 }
